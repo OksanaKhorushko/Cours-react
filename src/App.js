76ac07013c322dbe9c8301148a5dc064e67dab26
OksanaKhorushko <horushkosana@gmail.com>
2022-10-18 09:54:30 +0300

    Home work 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Message from './components/Message/Message.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Message message="Сообщение 1" />
+      <Message message="Другое" />
+    </div>
+  );
+}
+
+export default App;
